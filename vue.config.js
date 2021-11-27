ee60197c540aeeb638550f0a0bf22e719e6ac5bd
morgunove <morgunove87@gmail.com>
2021-11-27 17:08:39 +0500

    v1.3

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/VueCardApp/'
+    ? '/'
     : '/',
-  assetsDir: 'assets',
+  // assetsDir: 'assets',
   productionSourceMap: false
 }
